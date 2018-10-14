1afb91c25a4a2a7df5e19aa940d1508fce767986
jacekard <jacekard@gmail.com>
2018-10-14 22:06:08 +0200

    changed docker.sh file

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,3 +1,3 @@
 docker network create prestashop-net
 docker run -ti --name terrafelis-mysql --network prestashop-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
-docker run -ti --name terrafelis-prestashop --network prestashop-net -e DB_SERVER=terrafelis-mysql -p 8080:80 -d prestashop/prestashop
+docker run -ti --name terrafelis-prestashop --network prestashop-net -e DB_SERVER=terrafelis-mysql -e PS_DOMAIN=192.168.0.110:8080 -e PS_SHOP_URL=192.168.0.110:8080 -p 8080:80 -d prestashop/prestashop
